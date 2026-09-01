65bcaa0af7424fc3011d14e30fdf48bd4fa28e2b
Austin <austincraig0505@gmail.com>
2026-09-01 09:48:06 -1000

    Add png

--- contact_manager.py
+++ contact_manager.py
@@ -8,6 +8,9 @@ to practice using OOP and composition!
 """
 
 
+import json
+
+
 class ContactManager:
     """Class to do CRUD operations on the list of contacts"""
 
@@ -16,30 +19,46 @@ class ContactManager:
         self.contacts = []
 
     def load_contacts(self):
-        """Loads contacts from a JSON file and converts them to a list of
-        dictionaries
+        """Loads contacts from a JSON file and converts them to a list of dictionaries"""
 
-        Bonus: What should happen if the file isn't there?
-                What should happen if the file has invalid JSON in it?
-        """
-        return []
+        try:
+            with open(self.file, "r") as file:
+                self.contacts = json.load(file)
+        except FileNotFoundError:
+            self.contacts = []
+
+        return self.contacts
 
     def add_contact(self, contact):
         """Adds a contact to the list, and saves the file"""
-        pass
+
+        self.contacts.append(contact)
+
+        with open(self.file, "w") as file:
+            json.dump(self.contacts, file, indent=4)
 
     def update_contact(self, contact_to_update):
-        """
-        Updates a contact an saves the file
+        """Updates a contact and saves the file"""
 
-        Bonus: What happens when the id doesn't exist?
-        """
-        pass
+        for contact in self.contacts:
+            if contact["id"] == contact_to_update["id"]:
+                contact["name"] = contact_to_update["name"]
+                contact["email"] = contact_to_update["email"]
+                contact["phone"] = contact_to_update["phone"]
+
+                with open(self.file, "w") as file:
+                    json.dump(self.contacts, file, indent=4)
+
+                return
 
     def delete_contact(self, id_to_delete):
-        """
-        Deletes a contact and saves the file
+        """Deletes a contact and saves the file"""
 
-        Bonus: What happens when the id doesn't exist?
-        """
-        pass
+        for contact in self.contacts:
+            if contact["id"] == id_to_delete:
+                self.contacts.remove(contact)
+
+                with open(self.file, "w") as file:
+                    json.dump(self.contacts, file, indent=4)
+
+                return
